e243be605f1baf977fd7a9c133d93d31d85ab975
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-26 13:09:51 +0200

    Add project files.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzo
+    {
+        private string nome;
+        private string cognome;
+        private string via;
+        private string citta;
+        private string provincia;
+        private int codicePostale;
+
+        public Indirizzo(string nome, string cognome, string via, string citta, string provincia, int codicePostale)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.via = via;
+            this.citta = citta;    
+            this.provincia = provincia;
+            this.codicePostale = codicePostale;
+        }
+
+    }
+
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,41 @@
+﻿using ListaIndirizzi;
+string path = "C:\\Users\\Lorenzo\\Desktop\\Generation DOITA\\Tecnico\\Esercitazioni\\C#\\11 ListaIndirizzi\\ListaIndirizzi\\ListaIndirizzi\\addresses.csv";
+List<Indirizzo> indirizzi = new List<Indirizzo>();
+
+StreamReader fileIndirizziCSV = File.OpenText(path);
+
+fileIndirizziCSV.ReadLine();    //forse da controllare
+
+while (!fileIndirizziCSV.EndOfStream)
+{
+    string riga = fileIndirizziCSV.ReadLine();
+    string[] informazioni = riga.Split(',');
+    try
+    {
+        if(informazioni.Length == 6)
+        {
+            string nome = informazioni[0];
+            string cognome = informazioni[1];
+            string via = informazioni[2];
+            string citta = informazioni[3];
+            string provincia = informazioni[4];
+            int zip = int.Parse(informazioni[5]);
+
+            Indirizzo indirizzo = new Indirizzo(nome, cognome, via, citta, provincia, zip);
+            indirizzi.Add(indirizzo);
+        }
+        else
+        {
+            Console.WriteLine("Questo indirizzo non è scritto correttamente");
+        }
+    }catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+fileIndirizziCSV.Close();
+
+foreach(Indirizzo indirizzo in indirizzi)
+{
+    Console.WriteLine(indirizzo.ToString());
+}
